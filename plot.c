f3b024283cd8d2094993c1076cea86a3c479d5c8
umbe1987 <umbertofilippo@tiscali.it>
2025-06-09 17:37:24 +0200

    fixed data format that caused loop to stop at 255 iterations

--- plot.c
+++ plot.c
@@ -3,7 +3,8 @@
 
 #define X_MAX 100
 #define Y_MAX 100
-#define N 100		// number of agents
+#define N 1000		// number of agents
+#define K 100		// number of iteration
 
 struct Population {
     double type[N];
@@ -12,7 +13,7 @@ struct Population {
 };
 
 void initPop(struct Population *p) {
-    unsigned char i = 0;
+    unsigned int i = 0;
     while (i < N) {
         p->type[i] = rand() % 2;	// either 0 (if rand return is even), or 1 (otherwise)
         p->x[i] = rand() % X_MAX + 1;
@@ -33,15 +34,18 @@ int main() {
                     "set pointsize 2\n"						// set point size
                     "plot '-' using 1:2:3 palette\n");				// use inline data (http://gnuplot.info/docs_5.5/loc3515.html)
 
-    unsigned char i = 0;
+    unsigned int i = 0;
     while (i < N) {
         fprintf(gnuplot, "%g %g %g\n", pop.x[i], pop.y[i], pop.type[i]);
         i++;
     }
-    fprintf(gnuplot, "e\n");
+    // useful commands before closing (http://gnuplot.info/docs_5.5/loc7936.html)
+    fprintf(gnuplot, "bind all 'alt-End' 'exit gnuplot'\n"
+                     "pause mouse close\n"
+                     "e\n");
     fflush(gnuplot);
 
-    for (;;)
+    for (;;);
 
     pclose(gnuplot);
 
