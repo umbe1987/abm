ab32a1279a88a5bcab4d19193d8c6362a8c14221
umbe1987 <umbertofilippo@tiscali.it>
2025-06-09 16:15:21 +0200

    plot using array and color

--- plot.c
+++ plot.c
@@ -5,33 +5,43 @@
 #define Y_MAX 100
 #define N 100		// number of agents
 
-struct Agent {
-    unsigned char type;
-    double x;
-    double y;
+struct Population {
+    double type[N];
+    double x[N];
+    double y[N];
 };
 
-void initAgent(struct Agent *agent) {
-    agent->type = rand() % 2;		// either 0 (if rand return is even), or 1 (otherwise)
-    agent->x = rand() % X_MAX + 1;
-    agent->y = rand() % Y_MAX + 1;
-};
+void initPop(struct Population *p) {
+    unsigned char i = 0;
+    while (i < N) {
+        p->type[i] = rand() % 2;	// either 0 (if rand return is even), or 1 (otherwise)
+        p->x[i] = rand() % X_MAX + 1;
+        p->y[i] = rand() % Y_MAX + 1;
+        i++;
+    }
+}
 
 int main() {
 
+    struct Population pop;
+    initPop(&pop);
+
     FILE *gnuplot = popen("gnuplot", "w");
-    fprintf(gnuplot, "plot '-'\n");
+
+    // setup gnuplot
+    fprintf(gnuplot,"set palette model RGB defined ( 0 'blue', 1 'red' )\n"	// set color palette
+                    "set pointsize 2\n"						// set point size
+                    "plot '-' using 1:2:3 palette\n");				// use inline data (http://gnuplot.info/docs_5.5/loc3515.html)
+
     unsigned char i = 0;
     while (i < N) {
-        struct Agent a;
-        initAgent(&a);
-        fprintf(gnuplot, "%g %g\n", a.x, a.y);
+        fprintf(gnuplot, "%g %g %g\n", pop.x[i], pop.y[i], pop.type[i]);
         i++;
-    };
+    }
     fprintf(gnuplot, "e\n");
     fflush(gnuplot);
 
-    for (;;);
+    for (;;)
 
     pclose(gnuplot);
 
