92cbaaa27ecbb97f7086a67f542cf45d52ba34da
umbe1987 <umbertofilippo@tiscali.it>
2025-06-11 17:51:32 +0200

    global var for population and plot in a dedicated function

--- plot.c
+++ plot.c
@@ -12,33 +12,43 @@ struct Population {
     double y[N];
 };
 
-void initPop(struct Population *p) {
+struct Population pop;
+
+void initPop() {
     unsigned int i = 0;
     while (i < N) {
-        p->type[i] = rand() % 2;	// either 0 (if rand return is even), or 1 (otherwise)
-        p->x[i] = rand() % X_MAX + 1;
-        p->y[i] = rand() % Y_MAX + 1;
+        pop.type[i] = rand() % 2;	// either 0 (if rand return is even), or 1 (otherwise)
+        pop.x[i] = rand() % X_MAX + 1;
+        pop.y[i] = rand() % Y_MAX + 1;
         i++;
     }
+
+    return;
+}
+
+void updatePlot(FILE *gp) {
+
+    unsigned int i = 0;
+    while (i < N) {
+        fprintf(gp, "replot\n");
+        fprintf(gp, "%g %g %g\n", pop.x[i], pop.y[i], pop.type[i]);
+        i++;
+    }
+
+    return;
 }
 
 int main() {
 
-    struct Population pop;
-    initPop(&pop);
+    initPop(pop);
 
     FILE *gnuplot = popen("gnuplot", "w");
 
     // setup gnuplot
     fprintf(gnuplot,"set palette model RGB defined ( 0 'blue', 1 'red' )\n"	// set color palette
                     "set pointsize 2\n"						// set point size
-                    "plot '-' using 1:2:3 palette\n");				// use inline data (http://gnuplot.info/docs_5.5/loc3515.html)
-
-    unsigned int i = 0;
-    while (i < N) {
-        fprintf(gnuplot, "%g %g %g\n", pop.x[i], pop.y[i], pop.type[i]);
-        i++;
-    }
+                    "plot '-' using 1:2:3 palette\n");    			// use inline data (http://gnuplot.info/docs_5.5/loc3515.html)
+    updatePlot(gnuplot);
     // useful commands before closing (http://gnuplot.info/docs_5.5/loc7936.html)
     fprintf(gnuplot, "bind all 'alt-End' 'exit gnuplot'\n"
                      "pause mouse close\n"
