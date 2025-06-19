788f6ee96d58ba992dbe5c59c764ff3da02b9fec
umbe1987 <umbertofilippo@tiscali.it>
2025-06-19 17:57:13 +0200

    draft updates to pop and plots

--- plot.c
+++ plot.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define X_MAX 100
 #define Y_MAX 100
 #define N 1000		// number of agents
-#define K 100		// number of iteration
+#define K 10		// number of iteration
 
 struct Population {
-    double type[N];
-    double x[N];
-    double y[N];
+    unsigned int type[N];
+    unsigned int x[N];
+    unsigned int y[N];
 };
 
 struct Population pop;
@@ -17,18 +18,32 @@ struct Population pop;
 void initPop() {
     for (unsigned int i = 0; i < N; i++) {
         pop.type[i] = rand() % 2;	// either 0 (if rand return is even), or 1 (otherwise)
-        pop.x[i] = rand() % X_MAX + 1;
-        pop.y[i] = rand() % Y_MAX + 1;
     }
 
     return;
 }
 
-void updatePlot(FILE *gp) {
+void updatePop() {
     for (unsigned int i = 0; i < N; i++) {
-        fprintf(gp, "replot\n");
-        fprintf(gp, "%g %g %g\n", pop.x[i], pop.y[i], pop.type[i]);
+        pop.x[i] = rand() % X_MAX + 1;
+        pop.y[i] = rand() % Y_MAX + 1;
     }
+}
+
+void updatePlot(FILE *gp, unsigned int iter) {
+    FILE *data = fopen("data", "w+");
+    for (unsigned int i = 0; i < N; i++) {
+        fprintf(data, "%d %d %d\n", pop.x[i], pop.y[i], pop.type[i]);
+    }
+    fclose(data);
+
+    if (iter == 0) {
+        fprintf(gp, "plot 'data' using 1:2:3 palette\n");
+    } else {
+        fprintf(gp, "replot\n");
+    }
+    fflush(gp);
+    sleep(1);
 
     return;
 }
@@ -36,23 +51,18 @@ void updatePlot(FILE *gp) {
 int main() {
 
     initPop(pop);
-
-    FILE *gnuplot = popen("gnuplot", "w");
+    FILE *gp = popen("gnuplot -persist", "w");
 
     // setup gnuplot
-    fprintf(gnuplot,"set palette model RGB defined ( 0 'blue', 1 'red' )\n"	// set color palette
-                    "set pointsize 2\n"						// set point size
-                    "plot '-' using 1:2:3 palette\n");    			// use inline data (http://gnuplot.info/docs_5.5/loc3515.html)
-    updatePlot(gnuplot);
-    // useful commands before closing (http://gnuplot.info/docs_5.5/loc7936.html)
-    fprintf(gnuplot, "bind all 'alt-End' 'exit gnuplot'\n"
-                     "pause mouse close\n"
-                     "e\n");
-    fflush(gnuplot);
-
-    for (;;);
-
-    pclose(gnuplot);
+    fprintf(gp,"set palette model RGB defined ( 0 'blue', 1 'red' )\n"	// set color palette
+               "set pointsize 2\n"					// set point size
+               "set xrange[0:%d]\n"
+               "set yrange[0:%d]\n", X_MAX, Y_MAX);
+    for (unsigned int i = 0; i < K; i++) {
+        updatePop();
+        updatePlot(gp, i);
+    }
+    pclose(gp);
 
     return 0;
 }
